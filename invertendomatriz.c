06ba2b4e297259c71736b8788f428f339968c493
Anderson Basilio <120106202+AndersonBasilio@users.noreply.github.com>
2022-12-16 15:36:39 -0300

    Update invertendomatriz.c

--- invertendomatriz.c
+++ invertendomatriz.c
@@ -7,7 +7,7 @@ void desenhaBorda(){
 
 int main(){
     
-   int matriz[3][3], i, j, aux1, aux2;
+   int matriz[2][2], i, j, aux1, aux2;
 
         for(i = 0; i < 2; i++){
             for(j = 0; j < 2; j++){
